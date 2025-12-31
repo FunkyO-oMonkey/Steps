6ad62514b544d50be2755171903df13af82848f9
FunkyO-oMonkey <46450838+FunkyO-oMonkey@users.noreply.github.com>
2025-12-31 19:17:08 +0200

    Delete Alpha 0.4 directory

--- Alpha 0.4/sw.js
+++
@@ -1,9 +0,0 @@
-// This is the Service Worker
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // This allows the app to work offline later
-  e.respondWith(fetch(e.request));
-});
